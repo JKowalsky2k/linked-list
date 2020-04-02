f48ed484433774820c7752f09c2b253ee11f932f
Jedrzej Kowalski <253026@student.pwr.edu.pl>
2020-04-02 10:48:29 +0200

    Template class

--- list.h
+++ list.h
@@ -1,10 +1,10 @@
 #ifndef LIST_H
 #define LIST_H
 
-template <class type>
+template <typename T>
 class List {
 	struct Node {
-		type data;
+		T data;
 		Node *next;
 	};
 
@@ -13,13 +13,42 @@ class List {
 	Node * curr;
 	Node * temp;
 
-	bool isEmpty();
+//	bool isEmpty();
 
 public:
-	List(int data=0);
-	void addNode(int data);
-	void removeNode(int data);
-	void printList();
-};
+	List(T data) {
+		head = new Node;
+		head->next = nullptr;
+		head->data = data;
+		tail = head;
+		curr = nullptr;
+		temp = nullptr;
+	}
 
+	//~List();
+	void addNode(T data) {
+		Node * new_node = new Node;
+		new_node->data = data;
+		new_node->next = nullptr;
+		if (head->next != nullptr) {
+			curr = head;
+			while (curr->next != nullptr) {
+				curr = curr->next;
+			}
+			curr->next = new_node;
+		}
+		else 
+			head->next = new_node;
+	}
+
+	void printList() {
+		std::cout << "=======================\n";
+		curr = head;
+		while (curr != nullptr) {
+			std::cout << "Data: " << curr->data << "\n";
+			curr = curr->next;
+		}
+		std::cout << "=======================\n";
+	}
+};
 #endif
--- main.cpp
+++ main.cpp
@@ -2,9 +2,13 @@
 #include "list.h"
 
 int main() {
-	List l(0);
-	l.addNode(10);
-	l.addNode(20);
+	List < char > l('x');
+	l.addNode('a');
+	l.addNode('b');
 	l.printList();
+	List < int > l2(0);
+	l2.addNode(10);
+	l2.addNode(20);
+	l2.printList();
 	return 0;
 }
